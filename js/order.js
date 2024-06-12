3257190b70200422da47a8fa04e3e8f6e790cf18
kno1qq <aszxer705266@gmail.com>
2024-06-12 16:42:40 +0800

    1

---
+++ js/order.js
@@ -0,0 +1,9 @@
+// const form = document.querySelector("form");
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     const name = document.getElementById("name").value;
+//     const email = document.getElementById("email").value;
+//     const product = document.getElementById("product").value;
+//     alert(`訂購成功！\n姓名：${name}\n電子郵件：${email}\n產品：${product}`);
+//     form.reset();
+// });
